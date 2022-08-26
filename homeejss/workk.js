580734424269da72f88bd8134e29a56acfb72965
Rasulwebs <boboqulovrasul80@gamil.com>
2022-08-26 19:45:43 +0500

    home_js_masala

---
+++ homeejss/workk.js
@@ -0,0 +1,96 @@
+"use strict";
+
+
+// twoSum boyicha 
+// ========== 1 masala ==========
+
+// let nums = [3, 2, 4];
+// let target = 7;
+
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//         if (nums[i] + nums[j] == target) {
+
+//             console.log(i, j);
+//         }
+//     }
+// }
+
+// twoSum boyicha 
+// ========== 2 masala =============
+
+// let nums = [3, 2, 11, 3];
+// let target = 14;
+
+// for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//         if (nums[i] + nums[j] == target) {
+
+//             console.log(i, j);
+//         }
+//     }
+// }
+
+
+
+//  1 picture
+//  =========== 3 problem ===========
+
+// let fun = ['apple', 'cherry', 'limon'];
+
+
+// let n = 10;
+// let arr = []; // [1 , 2 , 8 , 16 , 32 , 64 , 128]
+// for(let i = 0; i<= n; i++){
+// arr[i] = Math.pow(2 , i);
+// }
+// console.log(arr)
+
+
+
+//  2 picture
+//  ============ 1 masala ===========
+
+//  istalgancha son uchun
+
+// console.log( 
+//     +prompt("Enter a value").split("").reverse().join("") 
+// )
+
+
+
+
+
+//  2 picture 
+// ========== 2 masala =============
+
+// let arr=[1, 2, 3, 4, 0, 5, 6, 7, 8, 8];
+// let num=0;
+
+// for(let key in arr){
+//     if(arr[key] == num){
+//         let x=arr.splice(key, 1);
+//         arr.push(x);
+//     }else{
+//         console.log();
+//     }
+// }
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
